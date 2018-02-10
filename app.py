ebfe483cd752ccf0bc62eb79d2a2c49a6d4f25ad
Nick Doyle <nick.p.doyle@gmail.com>
2018-02-10 17:58:16 +1100

    m

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     i=c=os.popen('curl -s http://169.254.169.254/latest/meta-data/instance-id/').read()
-    return "instance %s says Hello, Flask!" % i
+    return "Hello from instance %s" % i
 
 if __name__ == "__main__":
     app.run(port=80)
