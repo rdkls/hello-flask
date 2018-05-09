bf1a592c125c891449d3661c909aa0ba39890bea
Nick Doyle <nick.p.doyle@gmail.com>
2018-05-10 09:46:38 +1000

    enable local testing

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from flask import Flask
 import os
 
@@ -5,8 +6,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    i=c=os.popen('curl -s http://169.254.169.254/latest/meta-data/instance-id/').read()
+    i = 'local'
+    instanceid = os.popen('curl --max-time 1 -s http://169.254.169.254/latest/meta-data/instance-id/').read()
+    if instanceid:
+        i = instanceid
     return "Hello from instance %s!" % i
 
 if __name__ == "__main__":
-    app.run(port=80)
+    app.run(port=8888)
